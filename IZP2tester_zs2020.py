b019f97792eac23f92be2ee36a46def5314e613e
Adrijaned <adrijaned@gmail.com>
2020-11-25 19:08:28 +0100

    Valgrind tester, hopefully?

--- IZP2tester_zs2020.py
+++ IZP2tester_zs2020.py
@@ -11,19 +11,22 @@ class TestCase:
     expected_output: str
     actual_output: str
     name: str
+    valgrind_out: str = ""
 
-    def __init__(self, args: List[str], process_input: str, name: str, expected_output: str):
+    def __init__(self, args: List[str], process_input: str, name: str, expected_output: str, valgrind:bool=False):
         self.args = args
         self.process_input = process_input
         self.name = name
         self.expected_output = expected_output
+        self.valgrind = valgrind
 
     def run_test(self):
         with tempfile.NamedTemporaryFile() as test_input_file, open(self.process_input, 'rb') as source_input_file:
             test_input_file.write(source_input_file.read())
             test_input_file.flush()
             try:
-                subprocess.run(["./sps"] + self.args + [test_input_file.name], timeout=1000, check=True)
+                program_with_args = ["./sps"] + self.args + [test_input_file.name]
+                subprocess.run(program_with_args, timeout=1, check=True)
                 test_input_file.seek(0)
                 self.actual_output = test_input_file.read().decode("utf-8")
             except UnicodeDecodeError:
@@ -36,6 +39,11 @@ class TestCase:
                     self.actual_output = "SEGFAULT\n\n"
                 else:
                     self.actual_output = "ERROR\n\n"
+        if self.valgrind:
+            try:
+                self.valgrind_out = subprocess.run(['valgrind', '-q', '--leak-check=full'] + program_with_args, capture_output=True).stdout.decode('utf-8')
+            except:
+                pass
 
     def is_passed(self):
         if self.expected_output != 'ERROR':
@@ -43,7 +51,7 @@ class TestCase:
                 exp_out = resultFile.read()
         else:
             exp_out = 'ERROR\n\n'
-        return exp_out == self.actual_output
+        return exp_out == self.actual_output and self.valgrind_out == ''
 
     def get_log(self) -> str:
         printable_args = '"' + ('" "'.join(self.args)) + '"'
@@ -52,6 +60,7 @@ class TestCase:
                 exp_out = resultFile.read()
         else:
             exp_out = 'ERROR'
+        valgrind_log = "" if self.valgrind_out == "" else ("\nVALGRIND OUTPUT\n" + self.valgrind_out)
         return (f"----------------------\n"
                 f"Test case: {self.name}\n"
                 f"\n"
@@ -66,7 +75,8 @@ class TestCase:
                 f'{exp_out}END_OF_OUTPUT\n'
                 f'\n'
                 f'ACTUAL OUTPUT\n'
-                f'{self.actual_output}END_OF_OUTPUT\n')
+                f'{self.actual_output}END_OF_OUTPUT\n'
+                f'{valgrind_log}')
 
 def main():
     test_cases: List[TestCase] = []
@@ -76,13 +86,15 @@ def main():
         args = [test['cmds']]
         if test.get('delim'):
             args = ['-d', test['delim']] + args
-        test_cases += [TestCase(args, test['input'], test['name'], test['output'])]
+        test_cases += [TestCase(args, test['input'], test['name'], test['output'], '-mc' in sys.argv)]
 
     passedCount, wholeCount = 0, 0
     for test_case in test_cases:
+        wholeCount += 1
+        if wholeCount % 20 == 0 or (wholeCount % 5 == 0 and '-mc' in sys.argv):
+            print(f'Running test {wholeCount} of {len(test_cases)}')
         test_case.run_test()
         isPassed = test_case.is_passed()
-        wholeCount += 1
         if isPassed:
             passedCount += 1
         if not isPassed or '-v' in sys.argv:
